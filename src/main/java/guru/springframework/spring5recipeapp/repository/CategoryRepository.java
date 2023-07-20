b1243e90664b654fd42cce5689ef4b4275639482
Yuriy Tsarkov <yurait6@gmail.com>
2023-07-20 22:34:45 +0300

    add spring data query methods

--- src/main/java/guru/springframework/spring5recipeapp/repository/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5recipeapp.repository;
 
 import guru.springframework.spring5recipeapp.domain.Category;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,4 +9,6 @@ import org.springframework.data.repository.CrudRepository;
  * @author ytsarkov (yurait6@gmail.com) on 20.07.2023
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+  Optional<Category> findByDescription(String description);
 }
